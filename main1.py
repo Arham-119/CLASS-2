0ef3849327452b95971a7d5434d1991bd9ec19a2
Arham-119 <Luisak1980@gmail.com>
2026-07-21 10:57:18 -0700

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,27 @@
+# Assigning Different Variables
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+# Printing Different Variables and their Data Type
+print("Name :", name)
+print("Data type of name is", type(name))
+
+print("Age :", age)
+print("Data type of age is", type(age))
+
+print("Is Student :", is_student)
+print("Data type of is_student is", type(is_student))
+
+print("Weight :", weight)
+print("Data type of weight is", type(weight))
+
+# Type casting to convert the datatype of variables
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight is", type(weight))
