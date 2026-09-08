cda110adc039583c8a847bd12ee14d4325be2280
Arham-119 <Luisak1980@gmail.com>
2026-09-07 18:16:14 -0700

    Add files via upload

--- main36.py
+++ main36.py
@@ -1,16 +1,7 @@
-def factorial(x):
-    '''this is a recursive function to find the factorial of an integer'''
-
-    if x==0 or x==1:
-        return 1
+a = input ("Enter a word:")
+for i in a:
+    if (i == 'A'):
+        print("A is found")
+        break
     else:
-        #calling function inside a function
-        return x*factorial(x-1)
-
-#displaying result
-print(factorial.__doc__)
-print("the factorial of 0:",factorial(0))
-print("the factorial of 1:",factorial(1))
-print("the factorial of 2:", factorial(2))
-print("the factorial of 5:", factorial(5))
-print("the factorial of 10:", factorial(10))
+        print("A is not found")
